7b77795cfd04568c1cbc34915b010e138881ceb2
ahorguelin <antoinehorguelin@gmail.com>
2024-02-09 09:51:03 +0100

    sql khan init

---
+++ sql/lesson_1/book_list.sql
@@ -0,0 +1,9 @@
+CREATE TABLE books (
+id INTEGER PRIMARY KEY,
+name TEXT,
+rating INTEGER
+);
+
+INSERT INTO books VALUES (1, "Harry Potter", 10);
+INSERT INTO books VALUES (2, "A Study in Red", 8);
+INSERT INTO books VALUES (3, "Everything that remains", 9);
